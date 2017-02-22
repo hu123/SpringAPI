e3da86a8b285396984709210d9063be780ad38c1
huluo <huyunluo@gmail.com>
2017-02-22 22:20:39 +0800

    feat:初始化了Spring的开发环境

---
+++ src/test/java/Main.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/spring/ctx.xml")
+public class Main {
+
+    @Test
+    public void testName() throws Exception {
+
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.warn("我去");
+        System.out.println("呃呃呃");
+    }
+}
